a717fe4199dae62a7f4fc508970ae6edf1e67f55
Dushyant <144216671+Dushyantnagal@users.noreply.github.com>
2023-09-13 22:18:52 +0530

    Vowel or constant .py

---
+++ Vowel or Consonant.py
@@ -0,0 +1,8 @@
+val=input()
+if val=='a' or val=='e' or val=='i'or val=='o' or val=='u':
+    print('Vowel.')
+    
+elif 'a' <=val <='z' or 'A' <= val<='Z':
+        print('Consonant.')
+else:
+    print('Not an alphabet.')
