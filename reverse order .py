f8354dd545d17951d3156338736069aa12576bae
Dushyant <144216671+Dushyantnagal@users.noreply.github.com>
2023-09-18 20:56:31 +0530

    Reverse order.py

---
+++ reverse order .py
@@ -0,0 +1,8 @@
+def print_number_reverse(n):
+    
+    reversed_number = str(n)[::-1]
+    print("Number in reverse order:", reversed_number)
+
+n = int(input("Enter a number: "))
+
+print_number_reverse(n)
