e73b573867103c6b1a7a795c5b69cd9a397d2a6e
Dushyant <144216671+Dushyantnagal@users.noreply.github.com>
2023-11-22 13:40:50 +0530

    Remove key from dict.py

---
+++ remove key from dict.py
@@ -0,0 +1,9 @@
+marks = {'Physics': 12,
+         'Maths': 21,
+         'English':12,
+         'Webtech':13,
+         'ISF':15,
+         'Python':17
+         } 
+marks.pop('Python')
+print(marks)
