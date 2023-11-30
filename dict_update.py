0c04239dc1af5f84a726aef6a2d7540f6dd2cc27
Dushyant <144216671+Dushyantnagal@users.noreply.github.com>
2023-11-30 21:52:24 +0530

    Dict_update.py

---
+++ dict_update.py
@@ -0,0 +1,5 @@
+out = {'name': 'NA', 'mobile no.': 'NA', 'fathername': 'NA'}
+print('Before update', out)
+update_dict = {'name':'dushyant' ,'marks': 99}
+out.update(update_dict)
+print( 'After update ',out) 
